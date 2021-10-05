2849f030ed7416fec8cbbdd045246a5614f47c58
Elijah <72550846+ElijahBretbacher@users.noreply.github.com>
2021-10-05 10:45:52 +0200

    Implemented main()

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,14 @@
+/**
+ * @author Bretbacher Elijah
+ * @version Threads 1.0
+ * @date 05.10.2021
+ */
+public class Main {
+    public static void main(String[] args) {
+        DatePrinter dp = new DatePrinter();
+        CountPrinter cp = new CountPrinter();
 
+        dp.start();
+        cp.start();
+    }
+}
